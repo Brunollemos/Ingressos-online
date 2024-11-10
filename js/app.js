080d4d480c2b4838d57f43762f7af30d49b737c4
Bruno <brunollemos@live.com>
2024-11-10 15:30:00 -0300

    solução alternativa, código refatorado

--- js/app.js
+++ js/app.js
@@ -1,37 +1,53 @@
-let qtdPista = parseInt(document.getElementById('qtd-pista').innerHTML);
-let qtdSuperior = parseInt(document.getElementById('qtd-superior').innerHTML);
-let qtdInferior = parseInt(document.getElementById('qtd-inferior').innerHTML);
-limpar();
-
 function limpar() {
-    document.getElementById('qtd').value = '';    
+    document.getElementById('qtd').value = '';
 }
 
 function comprar() {
     let tipo = document.getElementById('tipo-ingresso');
-    let qtd = document.getElementById('qtd');
+    let qtd = parseInt(document.getElementById('qtd').value);
 
     if (tipo.value == 'pista') {
-        if (qtdPista < parseInt(qtd.value)) {
-            alert('Quantidade de ingressos indisponível');
-            return;
-        }
-        qtdPista -= parseInt(qtd.value);
-        document.getElementById('qtd-pista').innerHTML = qtdPista;
+        comprarPista(qtd);
     }else if (tipo.value == 'superior') {
-        if (qtdSuperior < parseInt(qtd.value)) {
-            alert('Quantidade de ingressos indisponível');
-            return;
-        }
-        qtdSuperior -= parseInt(qtd.value);
-        document.getElementById('qtd-superior').innerHTML = qtdSuperior;
-    }else {
-        if (qtdInferior < parseInt(qtd.value)) {
-            alert('Quantidade de ingressos indisponível');
-            return;
-        }
-        qtdInferior -= parseInt(qtd.value);
-        document.getElementById('qtd-inferior').innerHTML = qtdInferior;
-    }    
+        comprarSuperior(qtd);
+    }else if (tipo.value == 'inferior') {
+        comprarInferior(qtd);
+    }
     limpar();
+}
+
+function comprarPista(qtd) {
+    let qtdPista = parseInt(document.getElementById('qtd-pista').textContent);
+    if (qtd > qtdPista) {
+        alert('Quantidade de ingressos indisponível');
+        return;
+    }else {
+        qtdPista -= qtd;
+        document.getElementById('qtd-pista').textContent = qtdPista;
+        alert('Compra realizada com sucesso');
+    }
+}
+
+function comprarSuperior(qtd) {
+    let qtdSuperior = parseInt(document.getElementById('qtd-superior').textContent);
+    if (qtd > qtdSuperior) {
+        alert('Quantidade de ingressos indisponível');
+        return;
+    }else {
+        qtdSuperior -= qtd;
+        document.getElementById('qtd-superior').textContent = qtdSuperior;
+        alert('Compra realizada com sucesso');
+    }
+}
+
+function comprarInferior(qtd) {
+    let qtdInferior = parseInt(document.getElementById('qtd-inferior').textContent);
+    if (qtd > qtdInferior) {
+        alert('Quantidade de ingressos indisponível');
+        return;
+    }else {
+        qtdInferior -= qtd;
+        document.getElementById('qtd-inferior').textContent = qtdInferior;
+        alert('Compra realizada com sucesso');
+    }
 }
